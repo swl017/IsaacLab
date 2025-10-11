d6a965ffa70a9c78622d005bb72e33f1ac72b3c2
Seungwook Lee <seungwook1024@gmail.com>
2025-10-11 18:36:37 +0900

    Adding analytic triangulation covariance

--- source/isaaclab_tasks/isaaclab_tasks/direct/iris_ma/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/iris_ma/__init__.py
@@ -27,3 +27,16 @@ gym.register(
         "skrl_mappo_cfg_entry_point": f"{agents.__name__}:skrl_mappo_cfg.yaml",
     },
 )
+
+gym.register(
+    id="Isaac-Iris-MA-Direct-v1",
+    entry_point=f"{__name__}.iris_ma_env_tri_cov:IrisMAEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.iris_ma_env_tri_cov:IrisMAEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:IrisPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "skrl_mappo_cfg_entry_point": f"{agents.__name__}:skrl_mappo_cfg.yaml",
+    },
+)
