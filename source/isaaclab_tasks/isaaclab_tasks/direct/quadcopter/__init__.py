431d8bb17ff268c92f1bdcfb2248ba4928a43c82
Seungwook Lee <seungwook1024@gmail.com>
2026-01-22 17:09:08 +0900

    Added missed out gym env registration

--- source/isaaclab_tasks/isaaclab_tasks/direct/quadcopter/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/quadcopter/__init__.py
@@ -27,6 +27,18 @@ gym.register(
         "skrl_mappo_rnn_cfg_entry_point": f"{agents.__name__}:skrl_mappo_rnn_cfg.yaml",
     },
 )
+gym.register(
+    id="Isaac-Quadcopter-Direct-Delay-v0",
+    entry_point=f"{__name__}.quadcopter_env_delay:QuadcopterEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.quadcopter_env_delay:QuadcopterEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:QuadcopterPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg_delay.yaml",
+        "skrl_mappo_rnn_cfg_entry_point": f"{agents.__name__}:skrl_mappo_rnn_cfg.yaml",
+    },
+)
 
 gym.register(
     id="Isaac-Quadcopter-Direct-v1",
